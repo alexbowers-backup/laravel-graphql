60c07c0f7ed98ed3e823880c69718890b4ef96d8
Alex Bowers <bowersbros@gmail.com>
2017-04-27 00:03:23 +0100

    Base Schema

--- src/Schema.php
+++ src/Schema.php
@@ -2,7 +2,17 @@
 
 namespace AlexBowers\GraphQL;
 
+use Youshido\GraphQL\Schema\Schema as GraphQLSchema;
+
 class Schema
 {
+    public function build(array $schema)
+    {
+        // TODO: Convert the schema to Youshido's format
 
+        return new GraphQLSchema([
+            'query' => [],
+            'mutation' => [],
+        ]);
+    }
 }
